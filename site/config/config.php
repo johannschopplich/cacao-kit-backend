7c7c5006240348bf40eab323497a81e7b9b5c8f0
Johann Schopplich <mail@johannschopplich.com>
2023-10-25 10:41:54 +0200

    chore: stricter CORS allow headers

--- site/config/config.php
+++ site/config/config.php
@@ -69,7 +69,7 @@ return [
         'cors' => [
             'allowOrigin' => env('KIRBY_HEADLESS_ALLOW_ORIGIN', '*'),
             'allowMethods' => env('KIRBY_HEADLESS_ALLOW_METHODS', 'GET, POST, OPTIONS'),
-            'allowHeaders' => env('KIRBY_HEADLESS_ALLOW_HEADERS', '*'),
+            'allowHeaders' => env('KIRBY_HEADLESS_ALLOW_HEADERS', 'Accept, Content-Type, Authorization'),
             'maxAge' => env('KIRBY_HEADLESS_MAX_AGE', '86400')
         ]
     ]
